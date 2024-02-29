53ea5085abccadfe79dd0236bb1cc4334aefdf6e
Dewa <newdewa03@gmail.com>
2024-02-29 09:35:37 +0700

    pertemuan 4

--- pilem/lib/models/movie.dart
+++ pilem/lib/models/movie.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Movie {
   final int id;
+  final String title;
   final String overview;
   final String posterpath;
   final String backdropPath;
@@ -10,10 +11,22 @@ class Movie {
 
   Movie({
     required this.id,
+    required this.title,
     required this.overview,
     required this.posterpath,
     required this.backdropPath,
     required this.releaseDate,
     required this.voteAverage,
   });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+        id: json['id'],
+        title: json['title'],
+        overview: json['overview'],
+        posterpath: json["poster_path"],
+        backdropPath: json['poster_path'],
+        releaseDate: json['release_date'],
+        voteAverage: double.parse(json['vote_average']));
+  }
 }
