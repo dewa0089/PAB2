cf193f4f4f3ecf6cccb2195f118fe0c8f3eb42eb
dewa0089 <newdewa03@gmail.com>
2024-04-18 09:33:55 +0700

    push

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class NoteListScreen extends StatefulWidget {
@@ -15,7 +16,7 @@ class _NoteListScreenState extends State<NoteListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Notes')),
-      body: const Placeholder(),
+      body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
@@ -58,7 +59,22 @@ class _NoteListScreenState extends State<NoteListScreen> {
                         child: const Text('Cancel'),
                       ),
                     ),
-                    ElevatedButton(onPressed: () {}, child: const Text('Save'))
+                    ElevatedButton(
+                        onPressed: () {
+                          Map<String, dynamic> notes = {};
+                          notes['title'] = _titleController.text;
+                          notes['description'] = _descriptionController.text;
+
+                          FirebaseFirestore.instance
+                              .collection('notes')
+                              .add(notes)
+                              .whenComplete(() {
+                            _titleController.clear();
+                            _descriptionController.clear();
+                            Navigator.of(context).pop();
+                          });
+                        },
+                        child: const Text('Save'))
                   ],
                 );
               });
@@ -66,6 +82,57 @@ class _NoteListScreenState extends State<NoteListScreen> {
         tooltip: 'Add Notes',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class NoteList extends StatelessWidget {
+  const NoteList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          default:
+            return ListView(
+              padding: const EdgeInsets.only(bottom: 80),
+              children: snapshot.data!.docs.map((document) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                  child: Card(
+                    child: ListTile(
+                      onTap: () {},
+                      title: Text(document['title']),
+                      subtitle: Text(document['description']),
+                      trailing: InkWell(
+                        onTap: () {
+                          FirebaseFirestore.instance
+                              .collection('notes')
+                              .doc(document.id)
+                              .delete();
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Icon(Icons.delete),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
