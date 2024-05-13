9f602e9234b48df6e2e48150df1bbdcd034edfb5
dewa0089 <newdewa03@gmail.com>
2024-05-13 23:23:01 +0700

    push malam

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:notes/state_util.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/firebase_options.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Notes',
+        navigatorKey: Get.navigatorKey,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/services/note_services.dart';
+import 'package:notes/widgets/note_dialog.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -10,72 +10,23 @@ class NoteListScreen extends StatefulWidget {
 }
 
 class _NoteListScreenState extends State<NoteListScreen> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Notes')),
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
       body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Add'),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                          'Title',
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      TextField(
-                        controller: _titleController,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                          'Description',
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      TextField(
-                        controller: _descriptionController,
-                      )
-                    ],
-                  ),
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cancel'),
-                      ),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          NoteService.addNote(_titleController.text,
-                                  _descriptionController.text)
-                              .whenComplete(() {
-                            _titleController.clear();
-                            _descriptionController.clear();
-                            Navigator.of(context).pop();
-                          });
-                        },
-                        child: const Text('Save'))
-                  ],
-                );
-              });
+            context: context,
+            builder: (context) {
+              return NoteDialog();
+            },
+          );
         },
-        tooltip: 'Add Notes',
+        tooltip: 'Add Note',
         child: const Icon(Icons.add),
       ),
     );
@@ -102,83 +53,25 @@ class NoteList extends StatelessWidget {
             return ListView(
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.map((document) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
-                  child: Card(
-                    child: ListTile(
-                      onTap: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              final TextEditingController titleController =
-                                  TextEditingController(
-                                      text: document['title']);
-                              final TextEditingController
-                                  descriptionController = TextEditingController(
-                                      text: document['description']);
-                              return AlertDialog(
-                                content: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text('Update'),
-                                    const Padding(
-                                      padding: EdgeInsets.only(top: 10),
-                                      child: Text(
-                                        'Title',
-                                        textAlign: TextAlign.start,
-                                      ),
-                                    ),
-                                    TextField(
-                                      controller: titleController,
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.only(top: 10),
-                                      child: Text(
-                                        'Description',
-                                        textAlign: TextAlign.start,
-                                      ),
-                                    ),
-                                    TextField(
-                                      controller: descriptionController,
-                                    )
-                                  ],
-                                ),
-                                actions: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10),
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text('Cancel'),
-                                    ),
-                                  ),
-                                  ElevatedButton(
-                                      onPressed: () {
-                                        NoteService.updateNote(
-                                                document['id'],
-                                                titleController.text,
-                                                descriptionController.text)
-                                            .whenComplete(() =>
-                                                Navigator.of(context).pop());
-                                      },
-                                      child: const Text('Update'))
-                                ],
-                              );
-                            });
-                      },
-                      title: Text(document['title']),
-                      subtitle: Text(document['description']),
-                      trailing: InkWell(
-                        onTap: () {
-                          NoteService.deleteNote(document['id']);
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return NoteDialog(note: document);
                         },
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Icon(Icons.delete),
-                        ),
+                      );
+                    },
+                    title: Text(document.title),
+                    subtitle: Text(document.description),
+                    trailing: InkWell(
+                      onTap: () {
+                        NoteService.deleteNote(document);
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Icon(Icons.delete),
                       ),
                     ),
                   ),
